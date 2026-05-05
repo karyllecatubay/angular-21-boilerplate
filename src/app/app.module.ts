9c17d736239066140678d292a321c1c4e858cd1b
Karylle Catubay <karyllecatubigcatubay@gmail.com>
2026-05-05 18:01:28 +0800

    fix: correct initial setup issues

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { fakeBackendProvider } from './_helpers/fake-backend';
+import { AppRoutingModule } from './app-routing.module';         // ← direct file
+import { JwtInterceptor } from './_helpers/jwt.interceptor';
+import { ErrorInterceptor } from './_helpers/error.interceptor';
+import { appInitializer } from './_helpers/app.initializer';
+import { AccountService } from './_services/account.service';
+import { AppComponent } from './app.component';                  // ← direct file
+import { AlertComponent } from './_components/alert.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    AlertComponent,
+    HomeComponent
+  ],
+  providers: [
+    { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [AccountService] },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    fakeBackendProvider
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
